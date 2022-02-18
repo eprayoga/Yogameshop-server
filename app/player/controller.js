aaa6e405b7b78f1665cfefc3aad05e637ec6d9d8
Endang Prayoga Hidayatulloh <endangprayoga95@gmail.com>
2022-02-19 00:15:48 +0700

    Fixed History API

--- app/player/controller.js
+++ app/player/controller.js
@@ -168,8 +168,7 @@ module.exports = {
       ]);
 
       res.status(200).json({
-        data: history,
-        total: total.length ? total[0].value : 0,
+        data: { history, total: total.length ? total[0].value : 0 },
       });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
